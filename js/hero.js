114d31bb08477e94f6d04c8ef3ff381f33ff1182
Michael <43703002+michaelhoner@users.noreply.github.com>
2019-03-05 11:01:07 +0100

    5-3

---
+++ js/hero.js
@@ -0,0 +1,20 @@
+$(function hero() {
+  $(".typed-animation-1").typed({
+    strings: ["MICHAEL", "WEB"],
+    typeSpeed: 55,
+    startDelay: 1200,
+    backSpeed: 15,
+    backDelay: 2000,
+    showCursor: false,
+    loop: false
+  });
+  $(".typed-animation-2").typed({
+    strings: ["HONER", "DESIGNER", "CREATIVE", "DEVELOPER"],
+    typeSpeed: 70,
+    startDelay: 1200,
+    backSpeed: 30,
+    backDelay: 2000,
+    showCursor: false,
+    loop: false
+  });
+});
